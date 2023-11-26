d34785677607c0ce0e8184db01324e15dd76526d
kevinphillips11 <150522296+kevinphillips11@users.noreply.github.com>
2023-11-26 11:02:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+import requests
 
+app = Flask(__name__)
+CORS(app)
+
+NEWS_API_KEY = "b5b9a807cf6649128b07b112213b234d"  # Replace with your News API key
+
+@app.route('/')
+def index():
+    # Fetch top headlines from News API
+    url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={NEWS_API_KEY}'
+    response = requests.get(url)
+    news_data = response.json()
+
+    # Extract relevant information from the response
+    articles = news_data.get('articles', [])
+
+    return render_template('index.html', articles=articles)
+
+if __name__ == '__main__':
+    app.run(debug=True)
