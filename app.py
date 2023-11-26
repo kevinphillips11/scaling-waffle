4604cb68017a2023081acdb31ddeef5d9f1e3387
kevinphillips11 <150522296+kevinphillips11@users.noreply.github.com>
2023-11-26 11:50:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
 from flask_cors import CORS
 import requests
+import os
+from lastmileai import LastMile
+
+lastmile = LastMile(api_key=os.environ["LASTMILEAI_API_KEY"])
 
 app = Flask(__name__)
 CORS(app)
 
 NEWS_API_KEY = "b5b9a807cf6649128b07b112213b234d"  # Replace with your News API key
+
+@app.route('/v1/chat/completions', methods=['POST'])
+def chat_completions():
+    data = request.get_json()
+    messages = data['messages']
+    completion = lastmile.create_openai_chat_completion(
+      completion_params = {
+        model: "gpt-4",
+        messages: messages,
+      }
+    )
+    return completion
 
 @app.route('/')
 def index():
