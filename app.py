2b8a72580faa419cb2435b25240baea7984ce398
kevinphillips11 <150522296+kevinphillips11@users.noreply.github.com>
2023-11-30 16:15:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 import requests
 import os
 from lastmileai import LastMile
+import signal
+
 
 lastmile = LastMile(api_key=os.environ["LASTMILEAI_API_KEY"])
 
@@ -10,6 +12,17 @@ app = Flask(__name__)
 CORS(app)
 
 NEWS_API_KEY = "b5b9a807cf6649128b07b112213b234d"  # Replace with your News API key
+
+@app.route('/close_the_app')
+def close_the_app():
+    token = request.args.get('token')
+    if token == 'johnlocke':
+        print('Shutting down...')
+        os.kill(os.getpid(), signal.SIGINT)
+        return 'Server shutting down...'
+    else:
+        return 'Invalid token'
+
 
 @app.route('/v1/chat/completions', methods=['POST'])
 def chat_completions():
